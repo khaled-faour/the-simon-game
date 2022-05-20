d22f89d0b2b93695e8b27e428febeba11d85eedb
Khaled Faour <khaledfaour2@gmail.com>
2022-05-20 11:14:06 +0300

    Added addSequence Functionality

--- js/game.js
+++ js/game.js
@@ -1,8 +1,12 @@
+// Sequence pattern variable
+const pattern = [];
 
 // Get Next Sequence When 
 document.addEventListener('keydown', addSequence, {once: true});
 
 // Function to add sequence
 function addSequence(){
-    console.log("add sequence");
+    const randomNumber = Math.floor(Math.random()*4)
+    pattern.push(randomNumber)
+    console.log(pattern)
 }
