53099a8f4dc7ffbe18906c3d8ec4c7997e484e36
Khaled Faour <khaledfaour2@gmail.com>
2022-05-20 11:04:43 +0300

    Added Keydown EventListener

---
+++ js/game.js
@@ -0,0 +1,8 @@
+
+// Get Next Sequence When 
+document.addEventListener('keydown', addSequence, {once: true});
+
+// Function to add sequence
+function addSequence(){
+    console.log("add sequence");
+}
