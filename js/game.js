10da9c64388614bab151611d531eeae2a26162d8
Khaled Faour <khaledfaour2@gmail.com>
2022-05-20 11:19:07 +0300

    Added Sound Play

--- js/game.js
+++ js/game.js
@@ -1,12 +1,21 @@
 // Sequence pattern variable
 const pattern = [];
 
+// Colors array
+const colors = ["green", "red", "yellow", "blue"]
+
 // Get Next Sequence When 
 document.addEventListener('keydown', addSequence, {once: true});
 
 // Function to add sequence
 function addSequence(){
     const randomNumber = Math.floor(Math.random()*4)
-    pattern.push(randomNumber)
-    console.log(pattern)
+    pattern.push(colors[randomNumber])
+    playSound()
+}
+
+// Function to play audio of panel
+function playSound(){
+    const audio = new Audio(`../audio/${pattern[pattern.length - 1]}.mp3`)
+    audio.play()
 }
